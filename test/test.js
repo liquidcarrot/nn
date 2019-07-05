f7048cd17f880d89a6dd4d8787e783d258208966
luiscarbonell <luiscarbonell5683@gmail.com>
2019-07-05 19:20:52 -0400

    Added additional test and browser template

--- test/test.js
+++ test/test.js
@@ -2,4 +2,7 @@ describe("Something", function() {
   it("should be true", function() {
     expect(true).to.be.true;
   })
+  it("should be false", function() {
+    expect(false).to.be.false;
+  })
 })
