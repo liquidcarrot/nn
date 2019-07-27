6208a153bc210749617bb8b1ec8f5df81ccdf6a8
luiscarbonell <luiscarbonell5683@gmail.com>
2019-07-27 03:39:54 -0400

    Added Documentation, Testing, and Build Scripts

--- test/unit/test.js
+++
@@ -1,8 +0,0 @@
-describe("Something", function() {
-  it("should be true", function() {
-    expect(true).to.be.true;
-  })
-  it("should be false", function() {
-    expect(false).to.be.false;
-  })
-})
