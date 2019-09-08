2b0bd4b9df7f0b619562e693cc0d229b3c682d12
luiscarbonell5683 <luiscarbonell5683@gmail.com>
2019-09-07 23:56:17 -0400

    Added parallel webpack build & multiple module export formats

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
+// CHECK: https://tech.trivago.com/2015/12/17/export-multiple-javascript-module-formats/
 const path = require("path");
+const variants = require("parallel-webpack").createVariants;
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    "library": "NN",
-    "libraryTarget": "umd",
-    "path": path.resolve(__dirname, "dist"),
-    "filename": "bundle.js",
+function config(options) {
+  return {
+    "entry": "./src/index.js",
+    "output": {
+      "path": path.resolve(__dirname, "dist"),
+      "filename": `nn.${options.target}.js`,
+      "library": "NN",
+      "libraryTarget": options.target
+    },
+    "mode": "production"
   }
 }
+
+module.exports = variants({
+  target: ["window", "commonjs2", "amd", "umd2"]
+}, config);
